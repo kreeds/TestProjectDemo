dec0547208a092dcddad9ffe50a68abc315e03bc
Tan Jian Xin <tanjianxin@Admins-iMac.local>
2016-01-14 10:43:01 +0800

    - Character hair and clothes changing for editor

---
+++ Assets/Resources/Scripts/Character/CharacterMaker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMaker : MonoBehaviour {
+
+	[SerializeField]LAppModelProxy live2DModel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SelectHairLeft(){
+		live2DModel.ChangeHair (false);
+	}
+
+	void SelectHairRight(){
+		live2DModel.ChangeHair (true);
+	}
+
+	void SelectClothesLeft(){
+		live2DModel.ChangeClothes (false);
+	}
+	
+	void SelectClothesRight(){
+		live2DModel.ChangeClothes (true);
+	}
+}
