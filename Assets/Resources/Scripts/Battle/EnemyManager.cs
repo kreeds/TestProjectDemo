d6f58dc445a801c71398c1b6d8bf97a77ec4ce4f
Tan Jian Xin <tanjianxin@Admins-iMac.local>
2016-01-21 12:05:41 +0800

    - Complete fix for enemy appearance

--- Assets/Resources/Scripts/Battle/EnemyManager.cs
+++ Assets/Resources/Scripts/Battle/EnemyManager.cs
@@ -29,6 +29,8 @@ public class EnemyManager : MonoBehaviour {
 	void Start () 
 	{
 		GameObject obj = NGUITools.AddChild(m_panel.gameObject, Resources.Load("Prefabs/EnemySample") as GameObject);
+		obj.transform.rotation = Quaternion.Euler (270f, 0f, 0f);
+		obj.transform.localScale = new Vector3 (15, 15, 15);
 		currentEnemy = obj.GetComponent<Enemy>();
 		currentEnemy.transform.localPosition = new Vector3(146.0f, -95.0f, 0);
 		currentEnemy.Initialize(100, 100, 10, 2);
