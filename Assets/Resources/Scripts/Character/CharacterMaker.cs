8abfda51b66223914f4b32ea8433efdac7c230e4
Tan Jian Xin <tanjianxin@Admins-iMac.local>
2016-02-01 11:43:18 +0800

    - Edited the character customization scene

--- Assets/Resources/Scripts/Character/CharacterMaker.cs
+++ Assets/Resources/Scripts/Character/CharacterMaker.cs
@@ -3,11 +3,25 @@ using System.Collections;
 
 public class CharacterMaker : MonoBehaviour {
 
-	[SerializeField]LAppModelProxy live2DModel;
+	[SerializeField]LAppModelProxy 		live2DModel;
+	[SerializeField]UIInput				nameInput;
+
+	[SerializeField]UISprite			clothesBG;
+	[SerializeField]UISprite			hairBG;
+
+	public enum CustomizeState
+	{
+		Hair,
+		Clothes
+	}
+
+	private CustomizeState				currentState;
 
 	// Use this for initialization
 	void Start () {
-	
+		currentState = CustomizeState.Hair;
+		nameInput.label.text = "Gumi";
+		OnHairButton ();
 	}
 	
 	// Update is called once per frame
@@ -33,8 +47,36 @@ public class CharacterMaker : MonoBehaviour {
 
 	void SaveSettingsAndContinue(){
 		live2DModel.SaveProfile ();
+		PlayerProfile.Get ().playerName = nameInput.text;
 		Application.LoadLevel ("BattleScene");
 		//go to next scene
+	}
+
+	void OnClothesButton(){
+		currentState = CustomizeState.Clothes;
+		hairBG.gameObject.SetActive (false);
+		clothesBG.gameObject.SetActive (true);
+	}
+
+	void OnHairButton()
+	{
+		currentState = CustomizeState.Hair;
+		hairBG.gameObject.SetActive (true);
+		clothesBG.gameObject.SetActive (false);
+	}
+
+	void SelectLeft(){
+		if (currentState == CustomizeState.Hair)
+			live2DModel.ChangeHair (false);
+		else
+			live2DModel.ChangeClothes (false);
+	}
+	
+	void SelectRight(){
+		if (currentState == CustomizeState.Hair)
+			live2DModel.ChangeHair (true);
+		else
+			live2DModel.ChangeClothes (true);
 	}
 
 	void OnPlayAnimation()
