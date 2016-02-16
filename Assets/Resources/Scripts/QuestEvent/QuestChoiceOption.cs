8a01937cc14054ce305feedbe6b49bc4546960c4
Tan Jian Xin <tanjianxin@Admins-iMac.local>
2016-02-16 12:27:34 +0800

    - Event drama

---
+++ Assets/Resources/Scripts/QuestEvent/QuestChoiceOption.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestChoiceOption : MonoBehaviour {
+
+	private GameObject 				_rootObject;
+	private int						_choiceIndex;
+
+	[SerializeField]UILabel			_buttonText;
+
+	public void Initialize(GameObject rootObject, int choiceIndex, string buttonText){
+		_rootObject = rootObject;
+		_choiceIndex = choiceIndex;
+
+		_buttonText.text = buttonText;
+	}
+
+	void OnClick()
+	{
+		_rootObject.SendMessage ("OnChoiceSelected", _choiceIndex);
+	}
+}
