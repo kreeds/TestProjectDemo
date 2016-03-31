e26baaaac3ba764d72629a472d1a274953c9020c
Tan Jian Xin <tanjianxin@Admins-iMac.local>
2016-03-31 17:09:18 +0800

    - updated actionevent prefab

--- Assets/Resources/Scripts/QuestEvent/ActionEvent.cs
+++ Assets/Resources/Scripts/QuestEvent/ActionEvent.cs
@@ -4,10 +4,22 @@ using System.Collections;
 public class ActionEvent : MonoBehaviour {
 	[SerializeField]TweenScale		_expandTween;
 	[SerializeField]UILabel			_actionLabel;
+
 	[SerializeField]UISprite		_expandedBG;
+	[SerializeField]UISprite		_frameBG;
+	[SerializeField]UISprite		_fillBG;
+
+	[SerializeField]UISprite		_energyIcon;
+	[SerializeField]UILabel			_costLabel;
+
+	[SerializeField]UISprite		_baseBG;
+
 //	[SerializeField]UISlider		_progressBar;
 	[SerializeField]UIGauge			_progressGauge;
 	[SerializeField]BoxCollider		_buttonCollider;
+
+	[SerializeField]TweenColor		_alphaTween;
+	[SerializeField]TweenScale		_scaleTween;
 
 	private int				 		_actionId;
 	private GameObject				_rootObject;
@@ -21,6 +33,8 @@ public class ActionEvent : MonoBehaviour {
 	void Start () {
 		
 		_expandTween.Play (false);
+		_alphaTween.Play (false);
+		_scaleTween.Play (false);
 	}
 	
 	// Update is called once per frame
@@ -40,7 +54,7 @@ public class ActionEvent : MonoBehaviour {
 		_expandTween.Play (false);
 
 		Vector3 scale = _expandedBG.transform.localScale;
-		scale.x = (textScale.x * _actionLabel.transform.localScale.x) + 242;
+		scale.x = (textScale.x * _actionLabel.transform.localScale.x) + 150f;
 
 		expandedCenter = transform.localPosition;
 //		expandedCenter.x += scale.x / 4;
@@ -49,6 +63,19 @@ public class ActionEvent : MonoBehaviour {
 //		if (scale.x < 460)
 //			scale.x = 460;
 		_expandedBG.transform.localScale = scale;
+		_fillBG.transform.localScale = scale;
+
+		scale.y = _frameBG.transform.localScale.y;
+
+		_actionLabel.transform.localPosition = new Vector3 (-scale.x/2 + 40f, 0, -30f);
+		_fillBG.transform.localPosition = new Vector3 (-scale.x/2, 0);
+
+		_costLabel.transform.localPosition = new Vector3 (scale.x / 2 - 40f, 0, -12f);
+		_costLabel.text = required.ToString ();
+
+		_energyIcon.transform.localPosition = new Vector3 (scale.x / 2 - 60f, 0);
+
+		_frameBG.transform.localScale = scale;
 
 		_progressGauge.Init (0, required*10);
 
@@ -61,9 +88,17 @@ public class ActionEvent : MonoBehaviour {
 
 	void OnExpand()
 	{
+		_baseBG.enabled = false;
 		_isExpanded = !_isExpanded;
 		_expandTween.Play (_isExpanded);
+		_alphaTween.Play (_isExpanded);
+		_scaleTween.Play (_isExpanded);
 		_rootObject.SendMessage ("OnExpandAction", _actionId);
+	}
+
+	void OnExpandFinish()
+	{
+		_expandedBG.enabled = false;
 	}
 
 	void OnButtonClick()
